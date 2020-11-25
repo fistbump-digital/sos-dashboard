6fecb82bfcb20fd80334ebea4ccfa08e27a69313
fistbump-digital <74176421+fistbump-digital@users.noreply.github.com>
2020-11-25 23:35:19 +0530

    new build on 25/11

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sos-dashboard/precache-manifest.2167f00aeaff1a925da0b5b9acd4c2fc.js"
+  "/sos-dashboard/precache-manifest.d771d1c9ce1df6aa5c8621ddb929be18.js"
 );
 
 self.addEventListener('message', (event) => {
